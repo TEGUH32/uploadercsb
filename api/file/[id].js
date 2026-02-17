4a6842130cea00dc73d6307f7ebf0ff9e2875f61
TEGUH32 <teguhmarwin99@gmail.com>
2026-02-18 06:49:47 +0700

    Update [id].js

--- api/file/[id].js
+++ api/file/[id].js
@@ -1,22 +1,32 @@
 // =====================================================
-// GET FILE - SIMPLE VERSION
+// GET FILE API
 // =====================================================
-
-// Simple in-memory storage (temporary)
-const files = new Map();
 
 export default function handler(req, res) {
     const { id } = req.query;
     
     res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    if (!files.has(id)) {
-        return res.status(404).send('File not found');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
     }
 
-    const file = files.get(id);
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    // Get from global storage
+    if (!global.files) global.files = new Map();
     
+    const file = global.files.get(id);
+    
+    if (!file) {
+        return res.status(404).json({ error: 'File not found' });
+    }
+
     res.setHeader('Content-Type', file.type);
     res.setHeader('Content-Disposition', `inline; filename="${file.name}"`);
+    res.setHeader('Content-Length', file.buffer.length);
     res.send(file.buffer);
 }
