b90a52cd0440a771a09dbbe68869231d5ee6e153
TEGUH32 <teguhmarwin99@gmail.com>
2026-02-18 05:58:51 +0700

    Update [id].js

--- api/file/[id].js
+++ api/file/[id].js
@@ -1,19 +1,20 @@
 // =====================================================
-// SERVE FILE - IN MEMORY STORAGE
+// GET FILE - SIMPLE VERSION
 // =====================================================
 
-// Simple in-memory storage (will reset on each deploy)
-// Use a database for production
-const fileStorage = new Map();
+// Simple in-memory storage (temporary)
+const files = new Map();
 
 export default function handler(req, res) {
     const { id } = req.query;
     
-    if (!fileStorage.has(id)) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    if (!files.has(id)) {
         return res.status(404).send('File not found');
     }
 
-    const file = fileStorage.get(id);
+    const file = files.get(id);
     
     res.setHeader('Content-Type', file.type);
     res.setHeader('Content-Disposition', `inline; filename="${file.name}"`);
